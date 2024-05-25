43150a1c8a8829037c7f68ab328e30aea47cac96
Mohamad-Aassaf <mohamad1mohd@gmail.com>
2024-05-25 20:59:02 -0300

    falta só os estilos msm

--- script.js
+++ script.js
@@ -16,7 +16,8 @@ const portugueseContent = {
     otherInputLabel: 'Outros:',
     aceNotificationTitle: "Ace's:",
     resetButton: 'Resetar',
-    resultMessageLabel: 'Resultado:'
+    resultMessageLabel: 'Resultado:',
+    changeLanguage: 'Switch Language'
 };
 
 const englishContent = {
@@ -37,257 +38,60 @@ const englishContent = {
     otherInputLabel: 'Others:',
     aceNotificationTitle: "Ace's:",
     resetButton: 'Reset',
-    resultMessageLabel: 'Result:'
+    resultMessageLabel: 'Result:',
+    changeLanguage: 'Trocar Lingua'
 };
-
-const resultMessageTranslations = {
-    'SE PAGOU': 'YOU PAID HIMSELF',
-    'ME PARECE SER UM PROBLEMA DE HABILIDADE!': 'SKILL ISSUE DETECTED'
-};
-
 const listaDeTanks = [
-    {
-        nome: 'HT Peltast',
-        iconPath: 'assets/peltast.png',
-        preco: 23
-    },
-    {
-        nome: 'HT Javelin',
-        iconPath: 'assets/halftrack.png',
-        preco: 55
-    },
-    {
-        nome: 'AC Xiphos',
-        iconPath: 'assets/xiphos.png',
-        preco: 25
-    },
-    {
-        nome: 'AC Percutio',
-        iconPath: 'assets/percutio.png',
-        preco: 27
-    },
-    {
-        nome: 'AC Gemini',
-        iconPath: 'assets/gemini.png',
-        preco: 27
-    },
-    {
-        nome: 'Tankette Actaeon',
-        iconPath: 'assets/actaeon.png',
-        preco: 35
-    },
-    {
-        nome: 'Tankette Vesta',
-        iconPath: 'assets/vesta.png',
-        preco: 40
-    },
-    {
-        nome: 'Tankette Ixion',
-        iconPath: 'assets/ixion.png',
-        preco: 36
-    },
-    {
-        nome: 'Tankette Deioneus',
-        iconPath: 'assets/deioneus.png',
-        preco: 40
-    },
-    {
-        nome: 'LT Hatchet',
-        iconPath: 'assets/hatchet.png',
-        preco: 115
-    },
-    {
-        nome: 'LT Kranesca',
-        iconPath: 'assets/kranesca.png',
-        preco: 120
-    },
-    {
-        nome: 'LT Vulcan',
-        iconPath: 'assets/vulcan.png',
-        preco: 135
-    },
-    {
-        nome: 'LTD',
-        iconPath: 'assets/pelekys.png',
-        preco: 125
-    },
-    {
-        nome: 'MPT',
-        iconPath: 'assets/falchion.png',
-        preco: 54
-    },
-    {
-        nome: 'Spatha',
-        iconPath: 'assets/spatha.png',
-        preco: 75
-    },
-    {
-        nome: 'Bardiche',
-        iconPath: 'assets/bardiche.png',
-        preco: 165
-    },
-    {
-        nome: 'Ranseur',
-        iconPath: 'assets/ranseur.png',
-        preco: 180
-    },
-    {
-        nome: 'Scorpion',
-        iconPath: 'assets/scorpion.png',
-        preco: 160
-    },
-    {
-        nome: 'Ballista',
-        iconPath: 'assets/ballista.png',
-        preco: 155
-    },
-    {
-        nome: 'Talos',
-        iconPath: 'assets/talos.png',
-        preco: 60
-    },
-    {
-        nome: 'SPG',
-        iconPath: 'assets/spg.png',
-        preco: 1000
-    },
-    {
-        nome: 'BT',
-        iconPath: 'assets/bt.png',
-        preco: 900
-    },
-    {
-        nome: 'BTD',
-        iconPath: 'assets/btd.png',
-        preco: 1000
-    },
-    {
-        nome: 'SHT',
-        iconPath: 'assets/super.png',
-        preco: 2000
-    },
-    {
-        nome: 'FMG',
-        iconPath: 'assets/sagittarii.png',
-        preco: 20
-    },
-    {
-        nome: 'FAT 20mm',
-        iconPath: 'assets/fat20mmC.png',
-        preco: 25
-    },
-    {
-        nome: 'FAT Ram',
-        iconPath: 'assets/battering.png',
-        preco: 20
-    },
-    {
-        nome: 'FAT Smelter',
-        iconPath: 'assets/smelter.png',
-        preco: 30
-    },
-    {
-        nome: 'FAT Stygian',
-        iconPath: 'assets/stygian.png',
-        preco: 70
-    }
+    { "nome": "HT Peltast", "iconPath": "assets/peltast.png", "preco": 23 },
+    { "nome": "HT Javelin", "iconPath": "assets/halftrack.png", "preco": 55 },
+    { "nome": "AC Xiphos", "iconPath": "assets/xiphos.png", "preco": 25 },
+    { "nome": "AC Percutio", "iconPath": "assets/percutio.png", "preco": 27 },
+    { "nome": "AC Gemini", "iconPath": "assets/gemini.png", "preco": 27 },
+    { "nome": "Tankette Actaeon", "iconPath": "assets/actaeon.png", "preco": 35 },
+    { "nome": "Tankette Vesta", "iconPath": "assets/vesta.png", "preco": 40 },
+    { "nome": "Tankette Ixion", "iconPath": "assets/ixion.png", "preco": 36 },
+    { "nome": "Tankette Deioneus", "iconPath": "assets/deioneus.png", "preco": 40 },
+    { "nome": "LT Hatchet", "iconPath": "assets/hatchet.png", "preco": 115 },
+    { "nome": "LT Kranesca", "iconPath": "assets/kranesca.png", "preco": 120 },
+    { "nome": "LT Vulcan", "iconPath": "assets/vulcan.png", "preco": 135 },
+    { "nome": "LTD", "iconPath": "assets/pelekys.png", "preco": 125 },
+    { "nome": "MPT", "iconPath": "assets/falchion.png", "preco": 54 },
+    { "nome": "Spatha", "iconPath": "assets/spatha.png", "preco": 75 },
+    { "nome": "Bardiche", "iconPath": "assets/bardiche.png", "preco": 165 },
+    { "nome": "Ranseur", "iconPath": "assets/ranseur.png", "preco": 180 },
+    { "nome": "Scorpion", "iconPath": "assets/scorpion.png", "preco": 160 },
+    { "nome": "Ballista", "iconPath": "assets/ballista.png", "preco": 155 },
+    { "nome": "Talos", "iconPath": "assets/talos.png", "preco": 60 },
+    { "nome": "SPG", "iconPath": "assets/spg.png", "preco": 1000 },
+    { "nome": "BT", "iconPath": "assets/bt.png", "preco": 900 },
+    { "nome": "BTD", "iconPath": "assets/btd.png", "preco": 1000 },
+    { "nome": "SHT", "iconPath": "assets/super.png", "preco": 2000 },
+    { "nome": "FMG", "iconPath": "assets/sagittarii.png", "preco": 20 },
+    { "nome": "FAT 20mm", "iconPath": "assets/fat20mmC.png", "preco": 25 },
+    { "nome": "FAT Ram", "iconPath": "assets/battering.png", "preco": 20 },
+    { "nome": "FAT Smelter", "iconPath": "assets/smelter.png", "preco": 30 },
+    { "nome": "FAT Stygian", "iconPath": "assets/stygian.png", "preco": 70 },
 ];
 const listaDropdown = [
-    {
-        id: 1,
-        nome: 'O\'Brien V.110',
-        custo: 25
-    },
-    {
-        id: 2,
-        nome: 'O\'Brien Gravekeeper',
-        custo: 30
-    },
-    {
-        id: 3,
-        nome: 'O\'Brien Highlander',
-        custo: 26
-    },
-    {
-        id: 4,
-        nome: 'O\'Brien Wild Jack',
-        custo: 30
-    },
-    {
-        id: 5,
-        nome: 'O\'Brien Knave',
-        custo: 40
-    },
-    {
-        id: 6,
-        nome: 'O\'Brien Freeman',
-        custo: 40
-    },
-    {
-        id: 7,
-        nome: 'King Gallant',
-        custo: 72
-    },
-    {
-        id: 8,
-        nome: 'Devitt',
-        custo: 120
-    },
-    {
-        id: 9,
-        nome: 'Devitt Ironhide',
-        custo: 130
-    },
-    {
-        id: 10,
-        nome: 'Gallagher Outlaw',
-        custo: 150
-    },
-    {
-        id: 11,
-        nome: 'Gallagher Highwayman',
-        custo: 155
-    },
-    {
-        id: 12,
-        nome: 'Gallagher Thornfall',
-        custo: 165
-    },
-    {
-        id: 13,
-        nome: 'Silverhand',
-        custo: 155
-    },
-    {
-        id: 14,
-        nome: 'Silverhand Chieftain',
-        custo: 160
-    },
-    {
-        id: 15,
-        nome: 'Silverhand Lordscar',
-        custo: 370
-    },
-    {
-        id: 16,
-        nome: 'Noble Widow',
-        custo: 160
-    },
-    {
-        id: 17,
-        nome: 'Flood',
-        custo: 900
-    },
-    {
-        id: 18,
-        nome: 'Juggernaut',
-        custo: 1000
-    },
-    {
-        id: 19,
-        nome: 'Predator',
-        custo: 2000
-    },
+    { "id": 1, "nome": "O'Brien V.110", "custo": 25 },
+    { "id": 2, "nome": "O'Brien Gravekeeper", "custo": 30 },
+    { "id": 3, "nome": "O'Brien Highlander", "custo": 26 },
+    { "id": 4, "nome": "O'Brien Wild Jack", "custo": 30 },
+    { "id": 5, "nome": "O'Brien Knave", "custo": 40 },
+    { "id": 6, "nome": "O'Brien Freeman", "custo": 40 },
+    { "id": 7, "nome": "King Gallant", "custo": 72 },
+    { "id": 8, "nome": "Devitt", "custo": 120 },
+    { "id": 9, "nome": "Devitt Ironhide", "custo": 130 },
+    { "id": 10, "nome": "Gallagher Outlaw", "custo": 150 },
+    { "id": 11, "nome": "Gallagher Highwayman", "custo": 155 },
+    { "id": 12, "nome": "Gallagher Thornfall", "custo": 165 },
+    { "id": 13, "nome": "Silverhand", "custo": 155 },
+    { "id": 14, "nome": "Silverhand Chieftain", "custo": 160 },
+    { "id": 15, "nome": "Silverhand Lordscar", "custo": 370 },
+    { "id": 16, "nome": "Noble Widow", "custo": 160 },
+    { "id": 17, "nome": "Flood", "custo": 900 },
+    { "id": 18, "nome": "Juggernaut", "custo": 1000 },
+    { "id": 19, "nome": "Predator", "custo": 2000 }
 ];
 
 const counts = new Array(listaDeTanks.length).fill(0);
@@ -309,9 +113,11 @@ function toggleLanguage() {
     document.querySelector('#resetButton').textContent = content.resetButton;
 
     document.querySelector('.info h2').textContent = content.infoTitle;
+    document.querySelector('#languageSwitchButton').textContent = content.changeLanguage;
     document.querySelectorAll('.info p').forEach((p, index) => {
         p.textContent = content.infoText[index];
     });
+
     document.querySelector('.outro-Container label').textContent = content.otherInputLabel;
     document.querySelector('.aces').textContent = content.aceNotificationTitle;
     document.querySelector('#resetButton').textContent = content.resetButton;
